16bbe4dd7c0d940f9add52d116dfaf61e347e891
WeijieChen2017 <weijiechen1994@gmail.com>
2020-11-03 11:59:14 -0600

    v1

--- run.py
+++ run.py
@@ -1,17 +1,19 @@
 import os, argparse
 import numpy as np
+import nibabel as nib
 from tqdm import tqdm
 from glob import glob
-from PIL import Image
+# from PIL import Image
 
-import torch
-import torch.nn as nn
 
-import utils
-from EDSR.edsr import EDSR
-from modules import DSN
-from adaptive_gridsampler.gridsampler import Downsampler
-from skimage.color import rgb2ycbcr
+# import torch
+# import torch.nn as nn
+
+# import utils
+# from EDSR.edsr import EDSR
+# from modules import DSN
+# from adaptive_gridsampler.gridsampler import Downsampler
+# from skimage.color import rgb2ycbcr
 
 
 parser = argparse.ArgumentParser(description='Content Adaptive Resampler for Image downscaling')
@@ -23,75 +25,87 @@ parser.add_argument('--benchmark', type=bool, default=True, help='report benchma
 args = parser.parse_args()
 
 
-SCALE = args.scale
-KSIZE = 3 * SCALE + 1
-OFFSET_UNIT = SCALE
-BENCHMARK = args.benchmark
+# SCALE = args.scale
+# KSIZE = 3 * SCALE + 1
+# OFFSET_UNIT = SCALE
+# BENCHMARK = args.benchmark
 
-kernel_generation_net = DSN(k_size=KSIZE, scale=SCALE).cuda()
-downsampler_net = Downsampler(SCALE, KSIZE).cuda()
-upscale_net = EDSR(32, 256, scale=SCALE).cuda()
+# kernel_generation_net = DSN(k_size=KSIZE, scale=SCALE).cuda()
+# downsampler_net = Downsampler(SCALE, KSIZE).cuda()
+# upscale_net = EDSR(32, 256, scale=SCALE).cuda()
 
-kernel_generation_net = nn.DataParallel(kernel_generation_net, [0])
-downsampler_net = nn.DataParallel(downsampler_net, [0])
-upscale_net = nn.DataParallel(upscale_net, [0])
+# kernel_generation_net = nn.DataParallel(kernel_generation_net, [0])
+# downsampler_net = nn.DataParallel(downsampler_net, [0])
+# upscale_net = nn.DataParallel(upscale_net, [0])
 
-kernel_generation_net.load_state_dict(torch.load(os.path.join(args.model_dir, '{0}x'.format(SCALE), 'kgn.pth')))
-upscale_net.load_state_dict(torch.load(os.path.join(args.model_dir, '{0}x'.format(SCALE), 'usn.pth')))
-torch.set_grad_enabled(False)
+# kernel_generation_net.load_state_dict(torch.load(os.path.join(args.model_dir, '{0}x'.format(SCALE), 'kgn.pth')))
+# upscale_net.load_state_dict(torch.load(os.path.join(args.model_dir, '{0}x'.format(SCALE), 'usn.pth')))
+# torch.set_grad_enabled(False)
+
+def maxmin_norm(data):
+    MAX = np.amax(data)
+    MIN = np.amin(data)
+    SUM = np.sum(data, axis=-1)
+    if MAX-MIN > 0:
+        data = (data - MIN)/(MAX-MIN)
+    return data, SUM
 
 
 def validation(img, name, save_imgs=False, save_dir=None):
-    kernel_generation_net.eval()
-    downsampler_net.eval()
+    # kernel_generation_net.eval()
+    # downsampler_net.eval()
     upscale_net.eval()
 
-    kernels, offsets_h, offsets_v = kernel_generation_net(img)
-    downscaled_img = downsampler_net(img, kernels, offsets_h, offsets_v, OFFSET_UNIT)
-    downscaled_img = torch.clamp(downscaled_img, 0, 1)
-    downscaled_img = torch.round(downscaled_img * 255)
+    # kernels, offsets_h, offsets_v = kernel_generation_net(img)
+    # downscaled_img = downsampler_net(img, kernels, offsets_h, offsets_v, OFFSET_UNIT)
+    # downscaled_img = torch.clamp(downscaled_img, 0, 1)
+    # downscaled_img = torch.round(downscaled_img * 255)
 
-    reconstructed_img = upscale_net(downscaled_img / 255.0)
+    reconstructed_img = upscale_net(downscaled_img)
 
-    img = img * 255
-    img = img.data.cpu().numpy().transpose(0, 2, 3, 1)
-    img = np.uint8(img)
+    print(reconstructed_img.shape)
 
-    reconstructed_img = torch.clamp(reconstructed_img, 0, 1) * 255
-    reconstructed_img = reconstructed_img.data.cpu().numpy().transpose(0, 2, 3, 1)
-    reconstructed_img = np.uint8(reconstructed_img)
+    # img = img * 255
+    # img = img.data.cpu().numpy().transpose(0, 2, 3, 1)
+    # img = np.uint8(img)
 
-    downscaled_img = downscaled_img.data.cpu().numpy().transpose(0, 2, 3, 1)
-    downscaled_img = np.uint8(downscaled_img)
+    # reconstructed_img = torch.clamp(reconstructed_img, 0, 1) * 255
+    # reconstructed_img = reconstructed_img.data.cpu().numpy().transpose(0, 2, 3, 1)
+    # reconstructed_img = np.uint8(reconstructed_img)
 
-    orig_img = img[0, ...].squeeze()
-    downscaled_img = downscaled_img[0, ...].squeeze()
-    recon_img = reconstructed_img[0, ...].squeeze()
+    # downscaled_img = downscaled_img.data.cpu().numpy().transpose(0, 2, 3, 1)
+    # downscaled_img = np.uint8(downscaled_img)
 
-    if save_imgs and save_dir:
-        img = Image.fromarray(orig_img)
-        img.save(os.path.join(save_dir, name + '_orig.png'))
+    # orig_img = img[0, ...].squeeze()
+    # downscaled_img = downscaled_img[0, ...].squeeze()
+    # recon_img = reconstructed_img[0, ...].squeeze()
 
-        img = Image.fromarray(downscaled_img)
-        img.save(os.path.join(save_dir, name + '_down.png'))
+    # if save_imgs and save_dir:
+    #     img = Image.fromarray(orig_img)
+    #     img.save(os.path.join(save_dir, name + '_orig.png'))
 
-        img = Image.fromarray(recon_img)
-        img.save(os.path.join(save_dir, name + '_recon.png'))
+    #     img = Image.fromarray(downscaled_img)
+    #     img.save(os.path.join(save_dir, name + '_down.png'))
 
-    psnr = utils.cal_psnr(orig_img[SCALE:-SCALE, SCALE:-SCALE, ...], recon_img[SCALE:-SCALE, SCALE:-SCALE, ...], benchmark=BENCHMARK)
+    #     img = Image.fromarray(recon_img)
+    #     img.save(os.path.join(save_dir, name + '_recon.png'))
 
-    orig_img_y = rgb2ycbcr(orig_img)[:, :, 0]
-    recon_img_y = rgb2ycbcr(recon_img)[:, :, 0]
-    orig_img_y = orig_img_y[SCALE:-SCALE, SCALE:-SCALE, ...]
-    recon_img_y = recon_img_y[SCALE:-SCALE, SCALE:-SCALE, ...]
+    # psnr = utils.cal_psnr(orig_img[SCALE:-SCALE, SCALE:-SCALE, ...], recon_img[SCALE:-SCALE, SCALE:-SCALE, ...], benchmark=BENCHMARK)
 
-    ssim = utils.calc_ssim(recon_img_y, orig_img_y)
+    # orig_img_y = rgb2ycbcr(orig_img)[:, :, 0]
+    # recon_img_y = rgb2ycbcr(recon_img)[:, :, 0]
+    # orig_img_y = orig_img_y[SCALE:-SCALE, SCALE:-SCALE, ...]
+    # recon_img_y = recon_img_y[SCALE:-SCALE, SCALE:-SCALE, ...]
 
-    return psnr, ssim
+    # ssim = utils.calc_ssim(recon_img_y, orig_img_y)
+
+    # return psnr, ssim
+    return None
 
 
 if __name__ == '__main__':
-    img_list = glob(os.path.join(args.img_dir, '**', '*.png'), recursive=True)
+    img_list = glob("."+args.img_dir+'/*.nii')
+    print("."+args.img_dir+'/*.nii')
     assert len(img_list) > 0
 
     if not os.path.exists(args.output_dir):
@@ -103,11 +117,23 @@ if __name__ == '__main__':
         name = os.path.basename(img_file)
         name = os.path.splitext(name)[0]
 
-        img = utils.load_img(img_file)
+        print(img_file)
+        print(name)
+        print("--------------------")
 
-        psnr, ssim = validation(img, name, save_imgs=True, save_dir=args.output_dir)
-        psnr_list.append(psnr)
-        ssim_list.append(ssim)
+        nii_file = nib.load(img_file)
+        nii_data, total_sum = maxmin_norm(nii_file.get_fdata())
 
-    print('Mean PSNR: {0:.2f}'.format(np.mean(psnr_list)))
-    print('Mean SSIM: {0:.4f}'.format(np.mean(ssim_list)))
+        for idx_slice in range(nii_data.shape[2]):
+            nii_slice = nii_data[:, :, idx_slice]
+            validation(img, name, save_imgs=True, save_dir=args.output_dir)
+
+
+        # img = utils.load_img(img_file)
+
+    #     psnr, ssim = validation(img, name, save_imgs=True, save_dir=args.output_dir)
+    #     psnr_list.append(psnr)
+    #     ssim_list.append(ssim)
+
+    # print('Mean PSNR: {0:.2f}'.format(np.mean(psnr_list)))
+    # print('Mean SSIM: {0:.4f}'.format(np.mean(ssim_list)))
